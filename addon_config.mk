8a17c79b0986bd7110bb211052aebead6da17044
Dimitre <dimitre79@gmail.com>
2017-09-09 20:43:26 -0300

    updates

--- addon_config.mk
+++ addon_config.mk
@@ -1,9 +1,9 @@
 meta:
-	ADDON_NAME = ofxDmtrApa
-	ADDON_DESCRIPTION = Private Addon to handle DmtrApa Controller
+	ADDON_NAME = ofxPixelnode
+	ADDON_DESCRIPTION = PixelNode v0.01
 	ADDON_AUTHOR = Dimitre
 	ADDON_TAGS = "APA102" "APA" "LED" "LEDSTRIP"
-	ADDON_URL = http://github.com/dimitre/ofxDmtrApa
+	ADDON_URL = http://github.com/dimitre/ofxPixelnode
 
 common:
 	# dependencies with other addons, a list of them separated by spaces 
